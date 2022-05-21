2b32fdfec34997226b20bf8a2ccd30461af5e4d6
Jorge Tadeu <jsbatalha@gmail.com>
2022-05-20 23:23:00 -0300

    Trocando a cor do header

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -52,7 +52,7 @@ const Header = styled.header`
         align-items: center;
         width: 100%;
         min-height: 60px;
-        background-color: #f20732;
+        background-color: #008080;
         color: #fff;
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     div {
